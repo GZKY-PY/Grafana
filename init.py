ead9b48405e3e4873548de160a768978a788bc50
Py <42725991+yiwanggecaonima@users.noreply.github.com>
2019-09-09 03:43:19 +0800

    Create init.py

---
+++ init.py
@@ -0,0 +1,10 @@
+安装 influxdb
+wget https://dl.influxdata.com/influxdb/releases/influxdb_1.2.4_amd64.deb
+sudo dpkg -i influxdb_1.2.4_amd64.deb
+
+vi /etc/influxdb/influxdb.conf  配置文件
+
+influxd run 开启服务之后
+
+influx 进入交互模式
+
